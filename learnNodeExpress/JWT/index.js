5d225342d5de00949ae5a6e86b24b3fc25eb0b15
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-03-09 01:32:20 +0500

    Update index.js

--- learnNodeExpress/JWT/index.js
+++ learnNodeExpress/JWT/index.js
@@ -1,1 +1,57 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const port = 3000;
+
+const app = express();
+
+
+
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, "secretKey", (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Posts Uploaded...',
+                authData,
+            });
+        }
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'Muhibullah Khan',
+        email: 'muhib@gmail.com'
+    };
+
+    jwt.sign({ user: user }, 'security', (err, token) => {
+        res.json({
+            token,
+        });
+    });
+});
+
+
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello There'
+    })
+});
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization']
+    if (typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(' ')[1]
+        req.token = bearerToken
+        next()
+    } else {
+        res.sendStatus(403)
+    }
+}
+
+app.listen(port, (req, res) => {
+    console.log(`Server Started on ${port}`)
+});
